7e9203b5ddf9e4e6d67352402e4527dd486f3436
sabillaaa <salsabila.salsabila@students.amikom.ac.id>
2019-11-26 21:34:51 +0700

    Menambah code di main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello word')
+"""
+a = 5
+nama = 'sabil'
+umur = 19
+print('Nilai variabel a adalah ', a)
+print('Nama saya adalah &s!'&nama)
+print('%s is %d years old.' %(nama, umur))
+"""
+
+num = input('Masukkan umur kamu : ')
+print('Umur saya % tahun')
