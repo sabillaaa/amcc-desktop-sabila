d6d785f00d90c980f6c726a224e88cea7e0b1f77
sabil <salsabila.salsabila@students.amnikom.ac.id>
2019-12-08 23:29:01 +0700

    Menambah variabel

--- Basic.py
+++ Basic.py
@@ -9,3 +9,35 @@ print(date)
 number = 10
 string = 'Sabil'
 print(number, string)
+
+"""Simple Type"""
+x = 15
+y = '15'
+z = 15.1
+
+sum1 = x+x
+sum2 = y+y
+sum3 = z+z
+
+print(x,y,z)
+print(type(x),type(y),type(z))
+
+"""List"""
+print(list(range(1,10)))
+print(list(range(1,10,2)))
+
+"""Menjumlahkan nilai didalam list"""
+student_grades = [4,12.1,6,9]
+mysum = sum(student_grades)
+print(mysum)
+"""Panjang nilai didalam list"""
+length = len(student_grades)
+print(length)
+mean = mysum/length
+print(mean)
+"""Mencari jumlah nilai dalam list"""
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+print(student_grades.count(10.0))
+student = {'sabil':8.0, 'yanuar':7.0,'peby':9.0}
+print(student.values())
+print(student.keys())
