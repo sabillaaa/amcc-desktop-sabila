1ee475dfb218cb0972e4f06a2d45282ddc9be635
sabil <salsabila.salsabila@students.amnikom.ac.id>
2019-12-15 23:42:21 +0700

    Menambah file func.py

--- Basic.py
+++ Basic.py
@@ -46,8 +46,8 @@ dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
 print ("dict['Name']: ", dict['Name'])
 print ("dict['Class']: ", dict['Class'])
 
-import time; # Digunakan untuk meng-import modul time 
-ticks = time.time() 
+import time; # Digunakan untuk meng-import modul time
+ticks = time.time()
 print ("Berjalan sejak 12:00am, January 1, 1970:", ticks)
-localtime = time.localtime(time.time()) 
+localtime = time.localtime(time.time())
 print ("Waktu lokal saat ini :", localtime)
---
+++ func.py
@@ -0,0 +1,16 @@
+def square(x):
+    """Calculate Square Value"""
+    return x*x
+
+def foo(x, array):
+    if x in array:
+        return True
+    else:
+        return False
+
+print(foo(1,[1,2,3]))
+
+if (x, int) or dd(x, float) or x=='1':
+    print('hgbxh')
+else:
+    print('ggg')
