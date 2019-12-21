d9b24f466668ebef81bc1666f10be70b965415b2
sabil <salsabila.salsabila@students.amnikom.ac.id>
2019-12-21 15:16:23 +0700

    pertemuan desktop 4

---
+++ pert4.py
@@ -0,0 +1,71 @@
+# # dict = {'MNG':'5 kg','JRK':'10 kg','HMNG':'Rp 10.000','HJRK':'Rp 1.000'}
+
+# # print ("Andi Membeli Mangga", dict['MNG'], ", Jeruk", dict['JRK'])
+# # print ("Harga Mangga ", dict['HMNG'])
+# # print ("Harga Jeruk ", dict['HJRK'])
+
+# # def salam():
+# #     print ('Hello, Selamat Pagi')
+
+# # def luas_segitiga(alas, tinggi):
+# #     luas = (alas * tinggi) / 2
+# #     print ('Luas Segitiga : %f'%luas)
+
+# # def
+
+# # luas_segitiga(10, 65)
+# # salam()
+
+
+# def foo(x):
+#     return x*x
+
+# print (foo(3))
+
+#def luas_persegi(sisi):
+    #luas = (sisi * sisi)
+    #print ("Luas Persegi = %f"%luas)
+    
+#luas_persegi(4)'
+#'
+# def luaspersegi(sisi):
+#     return sisi * sisi
+
+# print(luaspersegi(12))
+
+# def segitiga(a,t):
+#     return (a*t)/2
+
+# print("luas segitiga :",segitiga(6,8))
+
+
+
+
+
+
+
+
+
+
+
+
+
+def Luas_Segitiga (alas,tinggi):
+
+    return ((alas*tinggi)/2)
+
+print ('Luas Segitiga %d'%Luas_Segitiga(6,8))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
